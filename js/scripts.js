5f13f31ed8c10e157c9b2618300c8ba34265c962
schance14 <schance.sd14@gmail.com>
2023-08-09 18:44:41 -0600

    Updated Css

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function newItem(){
+    let list = $('#list');
+    let inputValue = $('#input').val();
+    let li = $('<li></li>');
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('Please add something to the list first.');
+    } else {
+        list.append(li);
+    }
+
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+    li.on('dbclick', function crossOut() {
+        li.toggleClass('strike');
+    });
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('x'));
+    li.append(crossOutButton);
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem(){
+        li.addClass('delete');
+    }
+
+    $('#list').sortable();
+}
